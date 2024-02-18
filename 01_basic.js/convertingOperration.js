c72d1fba0eefd19b83a70ac88ae30a9bf87ecfdc
MukeshPatil198 <154341210+MukeshPatil198@users.noreply.github.com>
2024-02-18 11:48:08 +0000

    ConvertOperation

---
+++ 01_basic.js/convertingOperration.js
@@ -0,0 +1,122 @@
+//1> Number to all datatype
+/*let score = 2123;
+let valuescore = String(score); //2123
+let valuescore1 = Boolean(score);//true
+ //null is not a function Number value not convert the Null datarype because it is not function
+//undefind is not a function Number value not convert the undefind datarype because it is not function
+let valuescore2 = Symbol(score)//Symbol(2123)
+let valuescore3 = BigInt(score)//2123n
+console.log(valuescore3);
+console.log(typeof valuescore3);*/
+//###############################################3
+//2> string to all datatype
+/*let score = "Suraj Dhangar";
+let valuescore = Number(score); //NAN
+let valuescore1 = Boolean(score);//true
+ //null is not a function Number value not convert the Null datarype because it is not function
+//undefind is not a function Number value not convert the undefind datarype because it is not function
+let valuescore2 = Symbol(score)//Symbol(suraj dhangar)
+//Cannot convert Suraj Dhangar to a BigInt
+console.log(valuescore2);
+console.log(typeof valuescore2);*/
+//######################################################3
+//3> Null to all datatype
+/*let score = null
+let valuescore = Number(score); //0
+let valuescore1 = Boolean(score);//false
+let valuescore3 = String(score);//null
+ //null is not a function Number value not convert the Null datarype because it is not function
+//undefind is not a function Number value not convert the undefind datarype because it is not function
+let valuescore2 = Symbol(score)//Symbol(null)
+//Cannot convert Suraj Dhangar to a BigInt
+console.log(valuescore3);
+console.log(typeof valuescore3);*/
+//##################################################3
+
+// undefind to all datatype
+/*let score;
+let valuescore = String(score); //undefind
+let valuescore1 = Boolean(score);//false
+let valuescore3 = Number(score); //NaN
+ //null is not a function Number value not convert the Null datarype because it is not function
+//undefind is not a function Number value not convert the undefind datarype because it is not function
+let valuescore2 = Symbol(score)//Symbol()
+//Cannot convert undefined to a BigInt
+console.log(valuescore3);
+console.log(typeof valuescore3);*/
+//#################################################3
+//boolean to all datatype
+/*let boolean = true;
+let valuescore = String(boolean); //true
+let valuescore4 = Number(boolean);//1
+ //null is not a function Number value not convert the Null datarype because it is not function
+//undefind is not a function Number value not convert the undefind datarype because it is not function
+let valuescore2 = Symbol(boolean)//Symbol(true)
+let valuescore3 = BigInt(boolean)//1n
+console.log(valuescore2);
+console.log(typeof valuescore2);*/
+//####################################################3
+//BigInt to all datataype
+let score = BigInt(1234);
+let valuescore = String(score); //1234
+let valuescore4 = Number(score);//1234
+ //null is not a function Number value not convert the Null datarype because it is not function
+//undefind is not a function Number value not convert the undefind datarype because it is not function
+let valuescore2 = Symbol(score)//Symbol(1234)
+console.log(valuescore4);
+console.log(typeof valuescore4);
+//########################################################3
+/*  Number to all Datatype
+ 1.number to string = >123
+ 2.Number to Boolean => true
+ null is not a function Number value not convert the Null datarype because it is not function
+ undefind is not a function Number value not convert the undefind datarype because it is not function
+ 3.number to Symbbol => Symbol(123)
+ 4. Number to BigInt => 123n
+ //----------------------------------------------------------------------
+   String to all datatype
+ 1.String to Number = >NAN
+ 2.String to Boolean => true
+ null is not a function String value not convert the Null datarype because it is not function
+ undefind is not a function String value not convert the undefind datarype because it is not function
+ 3.String to Symbbol => Symbol("Suraj dhangar")
+ Cannot convert Suraj Dhangar to a BigInt
+ //---------------------------------------------------------------------
+    Null to all datatype
+ 1.Null to Number = >0
+ 2.Null to Boolean => false
+ 3.Null to String =>Null
+ null is not a function Null value not convert the Null datarype because it is not function
+ undefind is not a function Null value not convert the undefind datarype because it is not function
+ 4.Null to Symbbol => Symbol(null)
+ Cannot convert Suraj Dhangar to a BigInt
+ //-------------------------------------------------------------------------
+ undefind to all datatype
+ 1.Undefind to Number = >NAN
+ 2.Undefind to Boolean => false
+ 3.Undefind to String =>Undefind
+ null is not a function Undefind value not convert the Null datarype because it is not function
+ undefind is not a function Undefind value not convert the undefind datarype because it is not function
+ 4.Undefind to Symbol => Symbol()
+ Cannot convert Suraj Dhangar to a BigInt
+ //----------------------------------------------------------------------------
+  boolean to all datatype
+1.Boolean to Number = >1
+ 2.Boolean to String =>true
+ null is not a function Boolean value not convert the Null datarype because it is not function
+ Boolean is not a function Boolean value not convert the Boolean datarype because it is not function
+ 3.Boolean to Symbol => Symbol(true)
+ 4. Boolean to BigInt => 1n
+ //-------------------------------------------------------------------------------------------
+BigInt to all datataype
+1.BigInt to Number = >1234
+ 2.BigInt to String =>1234
+ null is not a function BigInt value not convert the Null datarype because it is not function
+ BigInt is not a function BigInt value not convert the BigInt datarype because it is not function
+ 3.BigInt to Symbol => Symbol(1234)
+ //------------------------------------------------------------------------*/
+
+
+
+
+
