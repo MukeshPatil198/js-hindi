0bf02e78e9d2fad2a3b76a0731e9cf07912ea903
MukeshPatil198 <154341210+MukeshPatil198@users.noreply.github.com>
2024-02-19 10:51:19 +0000

    01_Basic1

---
+++ 01_basic.js/07_datetime.js
@@ -0,0 +1,11 @@
+ let mydate = new Date();
+ console.log(mydate);
+ console.log(mydate.toString());
+ console.log(mydate.toDateString());
+ console.log(mydate.toJSON());
+ console.log(mydate.toLocaleDateString());
+ console.log(mydate.toLocaleString());
+ console.log(mydate.toTimeString());
+
+ let mycreatetime = new Date(2004,0,12);
+ console.log(mycreatetime.toISOString());
