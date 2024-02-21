06e702dc0fa439afe655b88174212b83f54cbd73
MukeshPatil198 <154341210+MukeshPatil198@users.noreply.github.com>
2024-02-21 21:32:59 +0000

    condition if,else,nesting

---
+++ 01_basic.js/04_controlflow.js/01_condition.js
@@ -0,0 +1,39 @@
+if (true) {
+    console.log("Hello");
+}
+
+const score12 = 23;
+const score1=21;
+if(score12 >score1)
+{
+  //console.log("yes");
+}
+if(score12 <=score1)
+{
+  console.log("yes");
+}
+//Operator => <,>,<=,>=,!=
+//###########################
+ const balance = 1000
+ if(balance < 500)
+ {
+    console.log("Hey");
+ }
+ else{
+    console.log("Hellow");
+ }
+ //##################################
+
+ //nesting condition
+
+  const score = 350;
+  if(score <50){
+      console.log("half century");
+  }
+  else if(score<100){
+    console.log("Century");
+  }
+  else{
+    console.log("this is a tripal century ");
+  }
+  //#####################################################
